3cabaaaf96f45b0425817c524e065d821d598835
Jean Menezes <76086877+JeanMenezees@users.noreply.github.com>
2021-11-30 12:20:51 -0300

    Introduzindo arquivos de projeto

---
+++ Raspadinha-Senac/js/app.js
@@ -0,0 +1,34 @@
+const cupons = [
+  {url: '../Imagens/Cupom20.png', codigo: '20OFFSNC'},
+  {url: '../Imagens/Cupom30.png', codigo: '30OFFSNC'},
+  {url: '../Imagens/Cupom50.png', codigo: '50OFFSNC'}
+];
+
+function pegarCupomAleatorio(){
+  return cupons[Math.floor(Math.random() * cupons.length)];
+}
+
+function copiarCodigo(){
+  var code = document.getElementById('codigo--raspadinha');
+
+  navigator.clipboard.writeText(code.value);
+
+  alert("Texto copiado:" + code.value);
+}
+
+var cupom = pegarCupomAleatorio();
+
+$('#raspadinha').wScratchPad({
+    size        : 50,          // The size of the brush/scratch.
+    bg          : cupom.url,  // Background (image path or hex color).
+    fg          : '../Imagens/CapaRaspadinha.png',  // Foreground (image path or hex color).
+    scratchMove : function(e, percent){
+      if(percent >= 80){
+        document.getElementById('codigo--raspadinha').value = cupom.codigo;
+        document.getElementById('codigo--raspadinha').style.display = "block";
+        document.getElementById('copiar').style.display = "block";
+        document.getElementById('pulso').style.display = "none";
+      }
+    },       // Set scratcMove callback.
+    cursor      : 'crosshair' // Set cursor.
+  });
